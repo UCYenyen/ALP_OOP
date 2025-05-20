a7e6d605019c9f7fc814c52765ec7d1f5040fee9
Obie Zuriel <obiezuriel631@gmail.com>
2025-05-20 16:08:48 +0700

    Add Job Class

---
+++ src/model/Job.java
@@ -0,0 +1,11 @@
+package model;
+
+public class Job {
+    private String name;
+    private double salary;
+
+    public Job(String name, double salary) {
+        this.name = name;
+        this.salary = salary;
+    }
+}
